3c53338f7c9f365a72e74617709cf6b1b6735076
funkiben <funkiben@gmail.com>
2020-07-29 17:27:31 -0400

    Added constants for standard headers

--- src/common/header.rs
+++ src/common/header.rs
@@ -48,16 +48,85 @@ macro_rules! headers {
 }
 
 headers! {
-    /// Connection header.
+    (ACCEPT, "accept");
+    (ACCEPT_CHARSET, "accept-charset");
+    (ACCEPT_ENCODING, "accept-encoding");
+    (ACCEPT_LANGUAGE, "accept-language");
+    (ACCEPT_RANGES, "accept-ranges");
+    (ACCESS_CONTROL_ALLOW_CREDENTIALS, "access-control-allow-credentials");
+    (ACCESS_CONTROL_ALLOW_HEADERS, "access-control-allow-headers");
+    (ACCESS_CONTROL_ALLOW_METHODS, "access-control-allow-methods");
+    (ACCESS_CONTROL_ALLOW_ORIGIN, "access-control-allow-origin");
+    (ACCESS_CONTROL_EXPOSE_HEADERS, "access-control-expose-headers");
+    (ACCESS_CONTROL_MAX_AGE, "access-control-max-age");
+    (ACCESS_CONTROL_REQUEST_HEADERS, "access-control-request-headers");
+    (ACCESS_CONTROL_REQUEST_METHOD, "access-control-request-method");
+    (AGE, "age");
+    (ALLOW, "allow");
+    (ALT_SVC, "alt-svc");
+    (AUTHORIZATION, "authorization");
+    (CACHE_CONTROL, "cache-control");
     (CONNECTION, "connection");
-    /// Content-Length header.
+    (CONTENT_DISPOSITION, "content-disposition");
+    (CONTENT_ENCODING, "content-encoding");
+    (CONTENT_LANGUAGE, "content-language");
     (CONTENT_LENGTH, "content-length");
-    /// Content-Type header.
+    (CONTENT_LOCATION, "content-location");
+    (CONTENT_RANGE, "content-range");
+    (CONTENT_SECURITY_POLICY, "content-security-policy");
+    (CONTENT_SECURITY_POLICY_REPORT_ONLY, "content-security-policy-report-only");
     (CONTENT_TYPE, "content-type");
-    /// Transfer-Encoding header.
-    (TRANSFER_ENCODING, "transfer-encoding");
-    /// Host header.
+    (COOKIE, "cookie");
+    (DNT, "dnt");
+    (DATE, "date");
+    (ETAG, "etag");
+    (EXPECT, "expect");
+    (EXPIRES, "expires");
+    (FORWARDED, "forwarded");
+    (FROM, "from");
     (HOST, "host");
+    (IF_MATCH, "if-match");
+    (IF_MODIFIED_SINCE, "if-modified-since");
+    (IF_NONE_MATCH, "if-none-match");
+    (IF_RANGE, "if-range");
+    (IF_UNMODIFIED_SINCE, "if-unmodified-since");
+    (LAST_MODIFIED, "last-modified");
+    (LINK, "link");
+    (LOCATION, "location");
+    (MAX_FORWARDS, "max-forwards");
+    (ORIGIN, "origin");
+    (PRAGMA, "pragma");
+    (PROXY_AUTHENTICATE, "proxy-authenticate");
+    (PROXY_AUTHORIZATION, "proxy-authorization");
+    (PUBLIC_KEY_PINS, "public-key-pins");
+    (PUBLIC_KEY_PINS_REPORT_ONLY, "public-key-pins-report-only");
+    (RANGE, "range");
+    (REFERER, "referer");
+    (REFERRER_POLICY, "referrer-policy");
+    (REFRESH, "refresh");
+    (RETRY_AFTER, "retry-after");
+    (SEC_WEBSOCKET_ACCEPT, "sec-websocket-accept");
+    (SEC_WEBSOCKET_EXTENSIONS, "sec-websocket-extensions");
+    (SEC_WEBSOCKET_KEY, "sec-websocket-key");
+    (SEC_WEBSOCKET_PROTOCOL, "sec-websocket-protocol");
+    (SEC_WEBSOCKET_VERSION, "sec-websocket-version");
+    (SERVER, "server");
+    (SET_COOKIE, "set-cookie");
+    (STRICT_TRANSPORT_SECURITY, "strict-transport-security");
+    (TE, "te");
+    (TRAILER, "trailer");
+    (TRANSFER_ENCODING, "transfer-encoding");
+    (USER_AGENT, "user-agent");
+    (UPGRADE, "upgrade");
+    (UPGRADE_INSECURE_REQUESTS, "upgrade-insecure-requests");
+    (VARY, "vary");
+    (VIA, "via");
+    (WARNING, "warning");
+    (WWW_AUTHENTICATE, "www-authenticate");
+    (X_CONTENT_TYPE_OPTIONS, "x-content-type-options");
+    (X_DNS_PREFETCH_CONTROL, "x-dns-prefetch-control");
+    (X_FRAME_OPTIONS, "x-frame-options");
+    (X_XSS_PROTECTION, "x-xss-protection");
 }
 
 /// Creates a map of headers.
