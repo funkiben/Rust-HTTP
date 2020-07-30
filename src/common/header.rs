fe1334cbddacc0d43093e647843c12688ba6d680
funkiben <funkiben@gmail.com>
2020-07-30 10:25:48 -0400

    Fixed header compilation error

--- src/common/header.rs
+++ src/common/header.rs
@@ -29,7 +29,7 @@ macro_rules! standard_headers {
     ) => {
         $(
             $(#[$docs])*
-            pub const $name: Header = Header::Predefined($value);
+            pub const $name: Header = Header::Standard($value);
         )+
 
         impl From<&str> for Header {
